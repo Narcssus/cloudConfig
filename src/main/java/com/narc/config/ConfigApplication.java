935a4f8c4b21778fba4e0b1ca76c674f463371ab
Narcssus <narcssusinnook@163.com>
2020-11-22 22:08:42 +0800

    fix bug

--- src/main/java/com/narc/config/ConfigApplication.java
+++ src/main/java/com/narc/config/ConfigApplication.java
@@ -2,12 +2,8 @@ package com.narc.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Administrator
